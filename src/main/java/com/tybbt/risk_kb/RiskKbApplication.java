62234b20a6827276d98ad5f786ca50420943f6d9
ty <875260315@qq.com>
2023-04-20 11:04:32 +0800

    init springboot

---
+++ src/main/java/com/tybbt/risk_kb/RiskKbApplication.java
@@ -0,0 +1,13 @@
+package com.tybbt.risk_kb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskKbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RiskKbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tybbt/risk_kb/RiskKbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tybbt.risk_kb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskKbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
