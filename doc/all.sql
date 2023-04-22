796541dc6ed2eaa83d7653d9ef241d86555be724
ty <875260315@qq.com>
2023-04-22 13:39:02 +0800

    mybatis创建风险类别

--- doc/all.sql
+++ doc/all.sql
@@ -1,6 +1,11 @@
-drop table if exists demo;
-create table demo (
+drop table if exists category;
+create table category (
                       id bigint not null comment 'id',
                       name varchar(50) comment '名称',
+                      parent bigint not null comment '父类',
+                      description varchar(100) comment '类别描述',
                       primary key (id)
-) engine=innodb default charset=utf8mb4 comment='测试';
+) engine=innodb default charset=utf8mb4 comment='风险类别';
+
+insert into category (id, name, parent, description) values (1, '税号误报风险知识', 0, '历史报关单证中存在的申报要素与税号的对应关系。');
+insert into category (id, name, parent, description) values (2, '商品要素误报风险知识', 0, '互联网大数据资源中的商品要素相关信息。');
--- src/main/java/com/tybbt/risk_kb/config/RiskKbApplication.java
+++ src/main/java/com/tybbt/risk_kb/config/RiskKbApplication.java
@@ -1,9 +1,13 @@
-package com.tybbt.risk_kb;
+package com.tybbt.risk_kb.config;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.tybbt")
 @SpringBootApplication
+@MapperScan("com.tybbt.risk.mapper")
 public class RiskKbApplication {
 
     public static void main(String[] args) {
